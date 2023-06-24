1fd9be564a0db0eb10e96698e27222e7602deef5
Alejandro Katcheroff <alejandro.katcheroff@scalablepath.com>
2023-06-24 18:16:02 -0300

    wip

--- app/Http/Controllers/Api/ApiMedic.php
+++ app/Http/Controllers/Api/ApiMedic.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Cache\RateLimiter;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
